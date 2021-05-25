d81cf8edd15b6eead82a1b35bc7df6c534bd6252
eetinosa <etinosa.enobun@gmail.com>
2021-05-25 12:27:48 +0100

    Scripts updated

--- Notebooks/AllCourses.py
+++ Notebooks/AllCourses.py
@@ -1,6 +1,7 @@
 #import libraries
 import requests
 import re
+import pandas as pd
 from bs4 import BeautifulSoup
 from requests import get
 base_url = "https://hackr.io/tutorials/learn-"
@@ -28,69 +29,70 @@ for i in range(len(coursenames)):
     # Pull all text from the tuturial divison that contains course listing
     courses = soup.find_all('div', class_ ="tut-list tut-list-new tut-row")
 
+
     
-for course in courses:
-    CourseTitle = course.div.find('span', class_ = 'tutorial-title-txt').text.strip()
-    CourseTitles.append(CourseTitle)
+    for course in courses:
+        CourseTitle = course.div.find('span', class_ = 'tutorial-title-txt').text.strip()
+        CourseTitles.append(CourseTitle)
 
 
-    NoOfVote  = course.div.find('span', class_ = 'count').text.strip()
-    Votes.append(NoOfVote)
+        NoOfVote  = course.div.find('span', class_ = 'count').text.strip()
+        Votes.append(NoOfVote)
 
-    Link1 = course.find("div", class_="title-links no-margin").find('a')["href"]
-    CourseLink.append(Link1)
-    
-    Name = course.find("a", class_="author no-marginright paddingleft-xs")
-    if Name is None:
-        AuthorName.append(None)
-    else:
-        Names=Name.text.strip()
-        AuthorName.append(Names)
-    
-    auth  = course.find("div", class_="action author").find('a')
-    if auth is None:
-        AuthorLink.append(None)
-    else:
-        Link2 = auth["href"]
-        AuthorLink.append(Link2)
-    
-    hackr = course.find("a", attrs={'href': re.compile("https://hackr.io/tutorial/")}) 
-    if hackr is None:
-        HackrLink.append(None)
-    else:
-        Link3=hackr.get("href")
-        HackrLink.append(Link3)
+        Link1 = course.find("div", class_="title-links no-margin").find('a')["href"]
+        CourseLink.append(Link1)
         
-    features  = course.find_all('div', class_ = "tut-label-group marginright-sm")
-    for future in features:
-        x=future.text.split()
-    feat1 = x[0]
-    if len(x) > 2:
-        feat2 = x[1]
-        feat3 = x[2]
-    elif x[1] == ("Beginner" or "Advanced"):
-        feat2 =None
-        feat3= x[1]
-    else:
-        feat2 = x[1]
-        feat3 = None
-    CourseCost.append(feat1)
-    CourseType.append(feat2)
-    StudentType.append(feat3)
+        Name = course.find("a", class_="author no-marginright paddingleft-xs")
+        if Name is None:
+            AuthorName.append(None)
+        else:
+            Names=Name.text.strip()
+            AuthorName.append(Names)
+        
+        auth  = course.find("div", class_="action author").find('a')
+        if auth is None:
+            AuthorLink.append(None)
+        else:
+            Link2 = auth["href"]
+            AuthorLink.append(Link2)
+        
+        hackr = course.find("a", attrs={'href': re.compile("https://hackr.io/tutorial/")}) 
+        if hackr is None:
+            HackrLink.append(None)
+        else:
+            Link3=hackr.get("href")
+            HackrLink.append(Link3)
+            
+        features  = course.find_all('div', class_ = "tut-label-group marginright-sm")
+        for future in features:
+            x=future.text.split()
+        feat1 = x[0]
+        if len(x) > 2:
+            feat2 = x[1]
+            feat3 = x[2]
+        elif x[1] == ("Beginner" or "Advanced"):
+            feat2 =None
+            feat3= x[1]
+        else:
+            feat2 = x[1]
+            feat3 = None
+        CourseCost.append(feat1)
+        CourseType.append(feat2)
+        StudentType.append(feat3)
 
+        CourseName.append(coursenames[i]) 
 
-import pandas as pd
-AllCourses = pd.DataFrame({'Course': CourseTitles,
-'Votes': Votes,
-"Course Link":CourseLink,
-"Author ":AuthorName,
-"Author Link":AuthorLink,
-"Hackr Link":HackrLink,
-"Course Cost":CourseCost,
-"Course Type":CourseType,
-"Course Cost":CourseCost,
-"Student Type":StudentType,
-"Course Name" :coursenames
-})
+        AllCourses = pd.DataFrame({'Course': CourseTitles,
+        'Votes': Votes,
+        "Course Link":CourseLink,
+        "Author ":AuthorName,
+        "Author Link":AuthorLink,
+        "Hackr Link":HackrLink,
+        "Course Cost":CourseCost,
+        "Course Type":CourseType,
+        "Course Cost":CourseCost,
+        "Student Type":StudentType,
+        "Course Names":CourseName,
+        })
 
-AllCourses.to_csv('AllCourses.csv')
+    AllCourses.to_csv('AllCourses.csv')
